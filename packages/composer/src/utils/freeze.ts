329f15ff477c1f9f15f77c9aa4544f6c30def76b
Tornike Tsomaia <ttsomaia@emis.ge>
2026-03-15 15:47:48 +0400

    Remove unused parameter from `BaseCompositionBridge` instantiation in `freeze`.

--- packages/composer/src/utils/freeze.ts
+++ packages/composer/src/utils/freeze.ts
@@ -3,7 +3,7 @@ import { FrozenClip } from '../interfaces/frozen-clip'
 import { BaseCompositionBridge } from '../composition/BaseCompositionBridge'
 
 export function freeze(composer: IClip): FrozenClip {
-  const bridge = new BaseCompositionBridge({})
+  const bridge = new BaseCompositionBridge()
   const composed = composer.compose(bridge)
   const recorder = new RecordingBridge()
 
