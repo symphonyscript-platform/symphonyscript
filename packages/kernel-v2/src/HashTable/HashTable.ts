2fc02daea503a0acdde1c7bb4681177137498622
Tornike Tsomaia <ttsomaia@emis.ge>
2026-03-23 20:54:59 +0400

    ProbeHashTable

--- packages/kernel-v2/src/HashTable/HashTable.ts
+++ packages/kernel-v2/src/HashTable/HashTable.ts
@@ -1,5 +1,7 @@
 export interface HashTable {
   readonly totalSizeInBytes: number
+  readonly endByteOffset: number
+
   get(key: number): number
   set(key: number, value: number): number
   delete(key: number): number
--- packages/kernel-v2/src/HashTable/ProbeHashTable.ts
+++ packages/kernel-v2/src/HashTable/ProbeHashTable.ts
@@ -5,6 +5,7 @@ import { ERROR_TABLE_FULL, OK } from '../constants'
 
 export class ProbeHashTable implements HashTable {
   public readonly totalSizeInBytes: number
+  public readonly endByteOffset: number
 
   private readonly capacity: number
   private readonly shift: number
@@ -18,67 +19,104 @@ export class ProbeHashTable implements HashTable {
     private readonly maxLoadFactor: number = 0.5,
     private readonly hash: HashingFunction = fibonacciHash,
   ) {
-    this.capacity = nextPowerOf2(Math.ceil(maxEntries / maxLoadFactor))
+    this.capacity = ProbeHashTable.calculateCapacity(maxEntries, maxLoadFactor)
+    this.totalSizeInBytes = ProbeHashTable.bytesRequired(maxEntries, maxLoadFactor)
+    this.endByteOffset = this.startByteOffset + this.totalSizeInBytes
+
     this.shift = 32 - Math.log2(this.capacity)
     this.sizeByteOffset = this.startByteOffset
     this.listStartByteOffset = this.startByteOffset + 4
-    this.totalSizeInBytes = this.maxEntries * 8 + 4 // +4 for size
     this.initializeSlots()
+  }
+
+  static calculateCapacity(maxEntries: number, maxLoadFactor: number) {
+    return nextPowerOf2(Math.ceil(maxEntries / maxLoadFactor))
+  }
+
+  static bytesRequired(maxEntries: number, maxLoadFactor: number) {
+    const capacity = ProbeHashTable.calculateCapacity(maxEntries, maxLoadFactor)
+    const slotSize = 12
+
+    return capacity * slotSize + 4 // +4 for "size" slot
   }
 
   get(key: number): number {
     const hash = this.hash(key, this.shift)
     const capacity = this.capacity
+    const mod = capacity - 1
     const start = this.listStartByteOffset
+    let displacement = 0
 
     for (let k = 0; k < capacity; ++k) {
-      const probe = k * (k + 1) / 2
-      const index = (hash + probe) & (capacity - 1)
-      const offset = (start >> 2) + index
-      const resolvedKey = this.sab[offset]
+      const index = (hash + k) & mod
+      const offset = (start >> 2) + index * 3
+      const slotKey = this.sab[offset + 1]
 
-      if (resolvedKey === -1) {
+      if (slotKey === -1) {
         return -1
       }
 
-      if (key === resolvedKey) {
-        return this.sab[offset + 1]
+      const slotHome = this.sab[offset] & mod
+      const slotDisplacement = (index - slotHome) & mod
+
+      if (displacement > slotDisplacement) {
+        return -1
       }
 
-      ++k
+      if (key === slotKey) {
+        return this.sab[offset + 2]
+      }
+
+      ++displacement
     }
 
     return -1
   }
 
   set(key: number, value: number): number {
-    const hash = this.hash(key, this.shift)
+    if (this.sab[this.sizeByteOffset >> 2] >= this.maxEntries) {
+      return ERROR_TABLE_FULL
+    }
+
     const capacity = this.capacity
+    const mod = capacity - 1
     const start = this.listStartByteOffset
-    let firstTombstoneOffset = -1
+    let hash = this.hash(key, this.shift)
+    let displacement = 0
 
     for (let k = 0; k < capacity; ++k) {
-      const probe = k * (k + 1) / 2
-      const index = (hash + probe) & (capacity - 1)
-      const offset = (start >> 2) + index
-      const resolvedKey = this.sab[offset]
+      const index = (hash + k) & mod
+      const offset = (start >> 2) + index * 3
+      const slotKey = this.sab[offset + 1]
 
-      if (resolvedKey === -1) {
-        const resolvedOffset = firstTombstoneOffset === -1
-          ? offset
-          : firstTombstoneOffset
-        this.sab[resolvedOffset] = key
-        this.sab[resolvedOffset + 1] = value
+      if (slotKey === -1) {
+        this.sab[offset] = hash
+        this.sab[offset + 1] = key
+        this.sab[offset + 2] = value
         this.sab[this.sizeByteOffset >> 2] += 1
 
         return OK
-      } else if (resolvedKey === -2 && firstTombstoneOffset === -1) {
-        firstTombstoneOffset = index
-        return OK
-      } else if (resolvedKey === key) {
-        this.sab[offset + 1] = value
+      } else if (slotKey === key) {
+        this.sab[offset + 2] = value
         return OK
       }
+
+      const slotHash = this.sab[offset]
+      const slotHome = slotHash & mod
+      const slotDisplacement = (index - slotHome) & mod
+
+      if (displacement > slotDisplacement) {
+        const slotValue = this.sab[offset + 2]
+        this.sab[offset] = hash
+        this.sab[offset + 1] = key
+        this.sab[offset + 2] = value
+        key = slotKey
+        value = slotValue
+        hash = slotHash
+        displacement = slotDisplacement
+      }
+
+      ++displacement
     }
 
     return ERROR_TABLE_FULL
@@ -92,8 +130,8 @@ export class ProbeHashTable implements HashTable {
     for (let k = 0; k < capacity; ++k) {
       const probe = k * (k + 1) / 2
       const index = (hash + probe) & (capacity - 1)
-      const offset = (start >> 2) + index
-      const resolvedKey = this.sab[offset]
+      const offset = (start >> 2) + index * 3
+      const resolvedKey = this.sab[offset + 1]
 
       if (key === -1) {
         return -1
@@ -103,6 +141,7 @@ export class ProbeHashTable implements HashTable {
         const value = this.sab[offset + 1]
         this.sab[offset] = -2
         this.sab[offset + 1] = -2
+        this.sab[offset + 2] = -2
         this.sab[this.sizeByteOffset >> 2] -= 1
 
         return value
@@ -118,9 +157,20 @@ export class ProbeHashTable implements HashTable {
 
   }
 
+  private backwardsShift(
+    deletedKey: number,
+    deletedIndex: number,
+  ) {
+    const capacity = this.capacity
+
+    for (let i = deletedIndex; i < capacity; ++i) {
+
+    }
+  }
+
   private initializeSlots() {
-    const end = this.listStartByteOffset + this.maxEntries * 8
-    this.sab.fill(-1, this.listStartByteOffset, end)
+    const end = this.endByteOffset
+    this.sab.fill(-1, this.listStartByteOffset >> 2, end >> 2)
     this.sab[this.sizeByteOffset >> 2] = 0
   }
 }
