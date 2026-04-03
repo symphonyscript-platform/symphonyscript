d3aea480b067e39122e99727582d1793ca0db26f
Tornike Tsomaia <ttsomaia@emis.ge>
2026-04-03 22:17:48 +0400

    changes

--- packages/kernel-rust/src/kernel_processor.rs
+++ packages/kernel-rust/src/kernel_processor.rs
@@ -1,7 +1,28 @@
-pub struct KernelProcessor {
+use crate::control_plane::ControlPlane;
+use crate::synaptic_graph_reader::SynapticGraphReader;
 
+pub struct KernelProcessor {
+    control_plane_ptr: *const ControlPlane,
 }
 
 impl KernelProcessor {
+    pub fn new(control_plane_address: usize) -> Self {
+        KernelProcessor {
+            control_plane_ptr: control_plane_address as *const ControlPlane,
+        }
+    }
 
+    pub fn acquire_graph(&mut self) -> &SynapticGraphReader {
+        let control_plane = unsafe { &*self.control_plane_ptr };
+        let graph_ptr = control_plane.get_shared_graph_ptr();
+        let mut graph = unsafe { &mut *graph_ptr };
+
+        graph.swap();
+
+        graph
+    }
+
+    pub fn process() {
+
+    }
 }
