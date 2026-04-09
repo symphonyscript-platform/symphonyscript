40ce39ebbdb94979e5367fb91fc51d416df3804b
Tornike Tsomaia <ttsomaia@emis.ge>
2026-04-09 13:18:05 +0400

    changes

--- packages/synaptic-kernel/src/graph_consumer.rs
+++ packages/synaptic-kernel/src/graph_consumer.rs
@@ -1,5 +1,6 @@
 use crate::control_plane::ControlPlane;
 use crate::synaptic_graph_reader::SynapticGraphReader;
+use std::sync::Arc;
 
 /// Consumer-side entry point to the graph reader.
 ///
@@ -22,41 +23,36 @@ use crate::synaptic_graph_reader::SynapticGraphReader;
 /// # Constraints
 /// - Created by passing `&ControlPlane` to `new()`.
 pub struct GraphConsumer<
-    'a,
     const NODE_META_SIZE: usize,
     const NODE_ATTRIBUTES_SIZE: usize,
     const SYNAPSE_META_SIZE: usize,
     const SYNAPSE_ATTRIBUTES_SIZE: usize,
 > {
-    control_plane: &'a ControlPlane<
-        NODE_META_SIZE,
-        NODE_ATTRIBUTES_SIZE,
-        SYNAPSE_META_SIZE,
-        SYNAPSE_ATTRIBUTES_SIZE,
-    >,
-}
-
-impl<
-    'a,
-    const NODE_META_SIZE: usize,
-    const NODE_ATTRIBUTES_SIZE: usize,
-    const SYNAPSE_META_SIZE: usize,
-    const SYNAPSE_ATTRIBUTES_SIZE: usize,
->
-    GraphConsumer<
-        'a,
-        NODE_META_SIZE,
-        NODE_ATTRIBUTES_SIZE,
-        SYNAPSE_META_SIZE,
-        SYNAPSE_ATTRIBUTES_SIZE,
-    >
-{
-    pub fn new(
-        control_plane: &'a ControlPlane<
+    control_plane: Arc<
+        ControlPlane<
             NODE_META_SIZE,
             NODE_ATTRIBUTES_SIZE,
             SYNAPSE_META_SIZE,
             SYNAPSE_ATTRIBUTES_SIZE,
+        >,
+    >,
+}
+
+impl<
+    const NODE_META_SIZE: usize,
+    const NODE_ATTRIBUTES_SIZE: usize,
+    const SYNAPSE_META_SIZE: usize,
+    const SYNAPSE_ATTRIBUTES_SIZE: usize,
+> GraphConsumer<NODE_META_SIZE, NODE_ATTRIBUTES_SIZE, SYNAPSE_META_SIZE, SYNAPSE_ATTRIBUTES_SIZE>
+{
+    pub fn new(
+        control_plane: Arc<
+            ControlPlane<
+                NODE_META_SIZE,
+                NODE_ATTRIBUTES_SIZE,
+                SYNAPSE_META_SIZE,
+                SYNAPSE_ATTRIBUTES_SIZE,
+            >,
         >,
     ) -> Self {
         GraphConsumer { control_plane }
