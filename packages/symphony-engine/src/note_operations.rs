0ebc0ee9d1611011a22bde5e7f7226d5c097cc9c
Tornike Tsomaia <ttsomaia@emis.ge>
2026-04-16 16:04:15 +0400

    Changes

--- packages/symphony-engine/src/note_operations.rs
+++ packages/symphony-engine/src/note_operations.rs
@@ -6,150 +6,150 @@ use crate::constants::{
 use crate::symphony_engine::SymphonyEngine;
 
 pub trait NoteOperations {
-    fn is_note_muted(&self, note: usize) -> bool;
-    fn mute_note(&self, note: usize);
-    fn unmute_note(&self, note: usize);
+    fn is_note_muted(&self, node: usize) -> bool;
+    fn mute_note(&self, node: usize);
+    fn unmute_note(&self, node: usize);
 
-    fn is_note_solo(&self, note: usize) -> bool;
-    fn set_note_solo(&self, note: usize);
-    fn unset_note_solo(&self, note: usize);
+    fn is_note_solo(&self, node: usize) -> bool;
+    fn set_note_solo(&self, node: usize);
+    fn unset_note_solo(&self, node: usize);
 
-    fn get_note_pitch(&self, note: usize) -> i32;
-    fn set_note_pitch(&self, note: usize, value: i32);
+    fn get_note_pitch(&self, node: usize) -> i32;
+    fn set_note_pitch(&self, node: usize, value: i32);
 
-    fn get_note_velocity(&self, note: usize) -> i32;
-    fn set_note_velocity(&self, note: usize, value: i32);
+    fn get_note_velocity(&self, node: usize) -> i32;
+    fn set_note_velocity(&self, node: usize, value: i32);
 
-    fn get_note_duration(&self, note: usize) -> i32;
-    fn set_note_duration(&self, note: usize, value: i32);
+    fn get_note_duration(&self, node: usize) -> i32;
+    fn set_note_duration(&self, node: usize, value: i32);
 
-    fn get_note_volume(&self, note: usize) -> i32;
-    fn set_note_volume(&self, note: usize, value: i32);
+    fn get_note_volume(&self, node: usize) -> i32;
+    fn set_note_volume(&self, node: usize, value: i32);
 
-    fn get_note_spatial_x(&self, note: usize) -> i32;
-    fn set_note_spatial_x(&self, note: usize, value: i32);
+    fn get_note_spatial_x(&self, node: usize) -> i32;
+    fn set_note_spatial_x(&self, node: usize, value: i32);
 
-    fn get_note_spatial_y(&self, note: usize) -> i32;
-    fn set_note_spatial_y(&self, note: usize, value: i32);
+    fn get_note_spatial_y(&self, node: usize) -> i32;
+    fn set_note_spatial_y(&self, node: usize, value: i32);
 
-    fn get_note_spatial_z(&self, note: usize) -> i32;
-    fn set_note_spatial_z(&self, note: usize, value: i32);
+    fn get_note_spatial_z(&self, node: usize) -> i32;
+    fn set_note_spatial_z(&self, node: usize, value: i32);
 
-    fn get_note_detune(&self, note: usize) -> i32;
-    fn set_note_detune(&self, note: usize, value: i32);
+    fn get_note_detune(&self, node: usize) -> i32;
+    fn set_note_detune(&self, node: usize, value: i32);
 
-    fn get_note_tick_offset(&self, note: usize) -> i32;
-    fn set_note_tick_offset(&self, note: usize, value: i32);
+    fn get_note_tick_offset(&self, node: usize) -> i32;
+    fn set_note_tick_offset(&self, node: usize, value: i32);
 }
 
 impl NoteOperations for SymphonyEngine {
-    fn is_note_muted(&self, note: usize) -> bool {
-        ((self.kernel.get_node_attribute(note, NOTE_ATTR_FLAGS) as u32) & (1 << NOTE_FLAG_MUTED))
+    fn is_note_muted(&self, node: usize) -> bool {
+        ((self.kernel.get_node_attribute(node, NOTE_ATTR_FLAGS) as u32) & (1 << NOTE_FLAG_MUTED))
             != 0
     }
 
-    fn mute_note(&self, note: usize) {
+    fn mute_note(&self, node: usize) {
         self.kernel
-            .or_node_attribute(note, NOTE_ATTR_FLAGS, 1 << NOTE_FLAG_MUTED);
+            .or_node_attribute(node, NOTE_ATTR_FLAGS, 1 << NOTE_FLAG_MUTED);
     }
 
-    fn unmute_note(&self, note: usize) {
+    fn unmute_note(&self, node: usize) {
         self.kernel
-            .and_node_attribute(note, NOTE_ATTR_FLAGS, !(1 << NOTE_FLAG_MUTED));
+            .and_node_attribute(node, NOTE_ATTR_FLAGS, !(1 << NOTE_FLAG_MUTED));
     }
 
-    fn is_note_solo(&self, note: usize) -> bool {
-        ((self.kernel.get_node_attribute(note, NOTE_ATTR_FLAGS) as u32) & (1 << NOTE_FLAG_SOLO))
+    fn is_note_solo(&self, node: usize) -> bool {
+        ((self.kernel.get_node_attribute(node, NOTE_ATTR_FLAGS) as u32) & (1 << NOTE_FLAG_SOLO))
             != 0
     }
 
-    fn set_note_solo(&self, note: usize) {
+    fn set_note_solo(&self, node: usize) {
         self.kernel
-            .or_node_attribute(note, NOTE_ATTR_FLAGS, 1 << NOTE_FLAG_SOLO);
+            .or_node_attribute(node, NOTE_ATTR_FLAGS, 1 << NOTE_FLAG_SOLO);
     }
 
-    fn unset_note_solo(&self, note: usize) {
+    fn unset_note_solo(&self, node: usize) {
         self.kernel
-            .and_node_attribute(note, NOTE_ATTR_FLAGS, !(1 << NOTE_FLAG_SOLO));
+            .and_node_attribute(node, NOTE_ATTR_FLAGS, !(1 << NOTE_FLAG_SOLO));
     }
 
-    fn get_note_pitch(&self, note: usize) -> i32 {
-        self.kernel.get_node_attribute(note, NOTE_ATTR_PITCH)
+    fn get_note_pitch(&self, node: usize) -> i32 {
+        self.kernel.get_node_attribute(node, NOTE_ATTR_PITCH)
     }
 
-    fn set_note_pitch(&self, note: usize, value: i32) {
-        self.kernel.set_node_attribute(note, NOTE_ATTR_PITCH, value);
+    fn set_note_pitch(&self, node: usize, value: i32) {
+        self.kernel.set_node_attribute(node, NOTE_ATTR_PITCH, value);
     }
 
-    fn get_note_velocity(&self, note: usize) -> i32 {
-        self.kernel.get_node_attribute(note, NOTE_ATTR_VELOCITY)
+    fn get_note_velocity(&self, node: usize) -> i32 {
+        self.kernel.get_node_attribute(node, NOTE_ATTR_VELOCITY)
     }
 
-    fn set_note_velocity(&self, note: usize, value: i32) {
+    fn set_note_velocity(&self, node: usize, value: i32) {
         self.kernel
-            .set_node_attribute(note, NOTE_ATTR_VELOCITY, value);
+            .set_node_attribute(node, NOTE_ATTR_VELOCITY, value);
     }
 
-    fn get_note_duration(&self, note: usize) -> i32 {
-        self.kernel.get_node_attribute(note, NOTE_ATTR_DURATION)
+    fn get_note_duration(&self, node: usize) -> i32 {
+        self.kernel.get_node_attribute(node, NOTE_ATTR_DURATION)
     }
 
-    fn set_note_duration(&self, note: usize, value: i32) {
+    fn set_note_duration(&self, node: usize, value: i32) {
         self.kernel
-            .set_node_attribute(note, NOTE_ATTR_DURATION, value);
+            .set_node_attribute(node, NOTE_ATTR_DURATION, value);
     }
 
-    fn get_note_volume(&self, note: usize) -> i32 {
-        self.kernel.get_node_attribute(note, NOTE_ATTR_VOLUME)
+    fn get_note_volume(&self, node: usize) -> i32 {
+        self.kernel.get_node_attribute(node, NOTE_ATTR_VOLUME)
     }
 
-    fn set_note_volume(&self, note: usize, value: i32) {
+    fn set_note_volume(&self, node: usize, value: i32) {
         self.kernel
-            .set_node_attribute(note, NOTE_ATTR_VOLUME, value);
+            .set_node_attribute(node, NOTE_ATTR_VOLUME, value);
     }
 
-    fn get_note_spatial_x(&self, note: usize) -> i32 {
-        self.kernel.get_node_attribute(note, NOTE_ATTR_SPATIAL_X)
+    fn get_note_spatial_x(&self, node: usize) -> i32 {
+        self.kernel.get_node_attribute(node, NOTE_ATTR_SPATIAL_X)
     }
 
-    fn set_note_spatial_x(&self, note: usize, value: i32) {
+    fn set_note_spatial_x(&self, node: usize, value: i32) {
         self.kernel
-            .set_node_attribute(note, NOTE_ATTR_SPATIAL_X, value);
+            .set_node_attribute(node, NOTE_ATTR_SPATIAL_X, value);
     }
 
-    fn get_note_spatial_y(&self, note: usize) -> i32 {
-        self.kernel.get_node_attribute(note, NOTE_ATTR_SPATIAL_Y)
+    fn get_note_spatial_y(&self, node: usize) -> i32 {
+        self.kernel.get_node_attribute(node, NOTE_ATTR_SPATIAL_Y)
     }
 
-    fn set_note_spatial_y(&self, note: usize, value: i32) {
+    fn set_note_spatial_y(&self, node: usize, value: i32) {
         self.kernel
-            .set_node_attribute(note, NOTE_ATTR_SPATIAL_Y, value);
+            .set_node_attribute(node, NOTE_ATTR_SPATIAL_Y, value);
     }
 
-    fn get_note_spatial_z(&self, note: usize) -> i32 {
-        self.kernel.get_node_attribute(note, NOTE_ATTR_SPATIAL_Z)
+    fn get_note_spatial_z(&self, node: usize) -> i32 {
+        self.kernel.get_node_attribute(node, NOTE_ATTR_SPATIAL_Z)
     }
 
-    fn set_note_spatial_z(&self, note: usize, value: i32) {
+    fn set_note_spatial_z(&self, node: usize, value: i32) {
         self.kernel
-            .set_node_attribute(note, NOTE_ATTR_SPATIAL_Z, value);
+            .set_node_attribute(node, NOTE_ATTR_SPATIAL_Z, value);
     }
 
-    fn get_note_detune(&self, note: usize) -> i32 {
-        self.kernel.get_node_attribute(note, NOTE_ATTR_DETUNE)
+    fn get_note_detune(&self, node: usize) -> i32 {
+        self.kernel.get_node_attribute(node, NOTE_ATTR_DETUNE)
     }
 
-    fn set_note_detune(&self, note: usize, value: i32) {
+    fn set_note_detune(&self, node: usize, value: i32) {
         self.kernel
-            .set_node_attribute(note, NOTE_ATTR_DETUNE, value);
+            .set_node_attribute(node, NOTE_ATTR_DETUNE, value);
     }
 
-    fn get_note_tick_offset(&self, note: usize) -> i32 {
-        self.kernel.get_node_attribute(note, NOTE_ATTR_TICK_OFFSET)
+    fn get_note_tick_offset(&self, node: usize) -> i32 {
+        self.kernel.get_node_attribute(node, NOTE_ATTR_TICK_OFFSET)
     }
 
-    fn set_note_tick_offset(&self, note: usize, value: i32) {
+    fn set_note_tick_offset(&self, node: usize, value: i32) {
         self.kernel
-            .set_node_attribute(note, NOTE_ATTR_TICK_OFFSET, value);
+            .set_node_attribute(node, NOTE_ATTR_TICK_OFFSET, value);
     }
 }
